60fd65a1e1de9f0b545de01234382238211fa62f
junilHwang <tjsdlf4261@naver.com>
2020-05-18 19:43:21 +0900

    feat: 북마크 조회 기능 작업 중 (2)

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -1,10 +1,24 @@
 import { BookmarkService } from '../services/index.js'
 
+const BookmarkItem = {
+  template: `
+    <article>
+      <h3>({{ id }}) {{ title }}</h3>
+    </article>
+  `,
+  props: [ 'title', 'children', 'id', 'index', 'parentId' ]
+}
+
 export const Bookmark = {
+  components: { BookmarkItem },
   template: `
     <section>
       <h2>북마크</h2>
-      {{ bookmarks }}
+      <bookmark-item
+        v-for="(item, k) in bookmarks.children"
+        :key="k"
+        v-bind="item"
+      />
     </section>
   `,
   data() {
