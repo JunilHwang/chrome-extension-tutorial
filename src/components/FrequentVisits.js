83f7fe2e4880dd5c5a15d97b5e08e4f1fe6e946b
junilhwang <tjsdlf4261@naver.com>
2020-05-27 16:45:44 +0900

    feat: 자주 방문한 사이트 개발 완료

--- src/components/FrequentVisits.js
+++ src/components/FrequentVisits.js
@@ -2,25 +2,35 @@ const microsecondsPerWeek = 1000 * 60 * 60 * 24 * 30;
 const startTime = Date.now() - microsecondsPerWeek;
 const maxResults = 1000
 
-chrome.history.search({ text: '', startTime, maxResults }, items => {
-  const urls = {}
-  for (const [ key, { url } ] of Object.entries(items)) {
-    (() => chrome.history.getVisits({ url }, result => {
-      for (const { transition } of result) {
-        if (transition === 'typed') urls[url] = ( urls[url] || 0 ) + 1;
-      }
-      if (~~key === 999) {
-        const urlArray = Object.keys(urls).sort((a, b) => urls[b] - urls[a])
-        console.log(urlArray);
-      }
-    }))();
-  }
-});
-
 export const FrequentVisits = {
   template: `
     <section>
       <h2>자주 방문한 사이트</h2>
+      <ul>
+        <li v-for="(v, k) in visited" :key="k">
+          <a :href="v" v-html="v" />
+        </li>
+      </ul>
     </section>
-  `
+  `,
+  data () {
+    return {
+      visited: []
+    }
+  },
+  created () {
+    chrome.history.search({ text: '', startTime, maxResults }, items => {
+      const urls = {}
+      for (const [ key, { url } ] of Object.entries(items)) {
+        (() => chrome.history.getVisits({ url }, result => {
+          for (const { transition } of result) {
+            if (transition === 'typed') urls[url] = ( urls[url] || 0 ) + 1;
+          }
+          if (~~key === 999) {
+            this.visited = Object.keys(urls).sort((a, b) => urls[b] - urls[a])
+          }
+        }))();
+      }
+    });
+  }
 }
