b077dd9d2dd02618c7b92a935f55a5825c48d216
junilhwang <tjsdlf4261@naver.com>
2020-05-28 09:47:58 +0900

    refactor: 자주 방문한 사이트 배열의 아이템의 구조 변경

--- src/components/FrequentVisits.js
+++ src/components/FrequentVisits.js
@@ -7,7 +7,7 @@ export const FrequentVisits = {
     <section>
       <h2>자주 방문한 사이트</h2>
       <ul>
-        <li v-for="([ url, { title } ]) in visited" :key="url" v-if="title.length">
+        <li v-for="({ url, title }) in visited" :key="url">
           <a :href="url" v-html="title" />
         </li>
       </ul>
@@ -23,7 +23,6 @@ export const FrequentVisits = {
       const visited = {}
       const sorted = (a, b) => b[1].count - a[1].count;
       chrome.history.search({text: '', startTime, maxResults}, items => {
-        console.log(items.map(v => v.title));
         items.forEach(({url, title}, key) => chrome.history.getVisits({url}, result => {
           result.forEach(({ transition }) => {
             if (transition === 'typed') {
@@ -31,9 +30,10 @@ export const FrequentVisits = {
               visited[url].count += 1;
             }
           })
-          if (key === 999) resolve(Object.entries(visited).sort(sorted))
+          if (key === 999) resolve(Object.entries(visited).sort(sorted).map(([ url, { title } ]) => ({ url, title })))
         }))
       })
     });
+    console.log(this.visited)
   }
 }
